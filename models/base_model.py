f3a0f38d2bb1345db40cab00358d6678869895d8
Codam <damisco005@gmail.com>
2023-05-17 00:54:03 +0100

    committed at Wed May 17 00:54:03 WAT 2023

--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,8 @@ class BaseModel():
     """Defines all common attributes/methods for other classes"""
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
 
     def __str__(self):
         return f'[BaseModel] ({self.id}) {self.__dict__}'
