e6a446f521c2bc3a599aa43eef14f1b3bcbe6167
Codam <damisco005@gmail.com>
2023-05-18 22:33:40 +0000

    Added some features

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+import storage
 """
         BaseModel that defines all common attributes/methods for other classes
 """
@@ -8,16 +9,27 @@ from datetime import datetime
 
 class BaseModel():
     """Defines all common attributes/methods for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs is not None and len(kwargs) > 0:
+            for k, v in kwargs.items():
+                if k == "__class__":
+                    continue
+                for i, j in self.__dict__.items():
+                    if i == k:
+                        self.__dict__[i] = v
+        else:
+            storage.new()
 
     def __str__(self):
         return f'[BaseModel] ({self.id}) {self.__dict__}'
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         new_dict = {}
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+import json
+""" class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances:
+
+    models/engine/file_storage.py"""
+
+class FileStorage():
+    """class FileStorage"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return __objects
+
+    def new(self, obj):
+        obj.__class__.name 
+
+    def save(self):
+        with open(__file_path, "w+") as tosave:
+            json.dump(__objects)
+
+    def reload(self):
+        with open(__file_path, "r") as tosave:
+            return json.load(__objects)
