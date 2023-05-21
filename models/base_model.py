4651875f71cb1ac883cf842938da8b77227a949e
Ajibose <ajiboseibrahim12@gmail.com>
2023-05-20 18:51:36 -0700

    Task4

--- models/base_model.py
+++ models/base_model.py
@@ -9,13 +9,13 @@ from datetime import datetime
 
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
-    def init(self):
+    def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return f'[{self.__class__.__name__}] (<{id}>) <{self.__dict__}>'
+        return f'[{self.__class__.__name__}] (<{self.id}>) <{self.__dict__}>'
 
     def save(self):
         self.updated_at = datetime.now()
