1b29da058d15715d9631f203881b526da163050a
Ajibose <ajiboseibrahim12@gmail.com>
2023-05-20 20:06:03 -0700

    Updated the BaseModel to create an instance from its dict

--- models/base_model.py
+++ models/base_model.py
@@ -9,10 +9,19 @@ from datetime import datetime
 
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            date_format = '%Y-%m-%dT%H:%M:%S.%f'
+            i_dict = kwargs.copy()
+            del i_dict["__class__"]
+            for key in i_dict:
+                if key == "created_at" or key == "updated_at":
+                    i_dict[key] = datetime.strptime(i_dict[key], date_format)
+            self.__dict__ = i_dict
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
