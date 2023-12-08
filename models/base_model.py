a7b35e30f1fcb5786cf812672a60b41de1f4728b
Codam <damisco005@gmail.com>
2023-12-08 07:33:06 +0100

    Engine, Object and Storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,9 @@
 #!/bin/python3.8
-"""class BaseModel that defines all common attributes/methods for other classes:"""
+"""class BaseModel that defines all common
+attributes/methods for other classes:"""
 from uuid import uuid4
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -10,26 +12,40 @@ class BaseModel:
     created_at: datetime
     updated_at: datetime
     """
-    def __init__(self):
+
+    def __init__(self, *args, **kwargs):
         """initializes id to a random id,
         created_at and updated_at to datetime respectively
         """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == '__class__':
+                    continue
+                if k[-3:] == '_at':
+                    v = datetime.fromisoformat(v)
+                self.__dict__[k] = v
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self.to_dict())
+
     def __str__(self):
         """
         should print [<class name>] (<self.id>) <self.__dict__>
         :return:
         """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
         """saves the content with the updated time"""
+        storage.save()
         self.updated_at = datetime.now()
+
     def to_dict(self, *args, **kwargs):
         """returns a dictionary containing all key/values of {self}"""
-        to_dict = self.__dict__
+        to_dict = self.__dict__.copy()
         to_dict['__class__'] = self.__class__.__name__
-        to_dict['updated_at'] = datetime.isoformat(to_dict['updated_at'])
         to_dict['created_at'] = datetime.isoformat(to_dict['created_at'])
+        to_dict['updated_at'] = datetime.isoformat(to_dict['updated_at'])
         return to_dict
